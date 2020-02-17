e53cb58ebda658db9ec02f080e0c9d11eeb1fd39
shreyadsouza <55857093+shreyadsouza@users.noreply.github.com>
2020-02-17 03:44:33 -0500

    updated js,readme

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Project Description
 
 <!-- you can include known bugs, design decisions, external references used... -->
+
+In this lab, we used CSS and HTML to style the webpage and JavaScript to do all the calculations so that the 
+page could be updated dynamically. 
+
+We use error checking in JavaScript to check that the inputs are valid and then check that the inputs can form 
+a triangle. We factor out the code into different functions that calculate the various properties of the triangle. 
+
+We take into account accessibility requirements by scaling the text dynamically and use skip links and a logical page hierarchy. 
--- triangle.js
+++ triangle.js
@@ -1,41 +1,44 @@
-// create function that stores variable and writes answers
-// when writing answers call on different functions within the script
-
 function calculate(){
-    var length1 = Number(document.getElementById('length1').value);
-    var length2 = Number(document.getElementById('length2').value);
-    var length3 = Number(document.getElementById('length3').value);
-    if (checkValid(length1, length2, length3) == 1) {
-        document.getElementById("triangle-type").innerHTML = triangleType(length1, length2, length3);
-        var perimeter_var = perimeter(length1, length2, length3);
-        document.getElementById("perimeter").innerHTML = perimeter_var;
-        document.getElementById("area").innerHTML = area(length1, length2, length3, perimeter_var);
-        var ang1 = angles(length1, length2, length3);
-        var ang2 = angles(length2, length3, length1);
-        var ang3 = angles(length3, length1, length2);
-        document.getElementById("angle-1").innerHTML = ang1;
-        document.getElementById("angle-2").innerHTML = ang2;
-        document.getElementById("angle-3").innerHTML = ang3;
-        document.getElementById("triangle-type-2").innerHTML = triangleTypeAng(ang1, ang2, ang3);
-
-
+    if (isNaN(document.getElementById('length1').value) || isNaN(document.getElementById('length2').value) || isNaN(document.getElementById('length3').value)){
+        document.getElementById("triangle-type").innerHTML = "NON-NUMBER INPUT";
+        document.getElementById("perimeter").innerHTML = "NON-NUMBER INPUT";
+        document.getElementById("area").innerHTML = "NON-NUMBER INPUT";
+        document.getElementById("angle-1").innerHTML = "NaN";
+        document.getElementById("angle-2").innerHTML = "NaN";
+        document.getElementById("angle-3").innerHTML = "NaN";
+        document.getElementById("triangle-type-2").innerHTML = "NON-NUMBER INPUT";
     } else {
-        document.getElementById("triangle-type").innerHTML = "INVALID LENGTHS";
-        document.getElementById("perimeter").innerHTML = "INVALID LENGTHS";
-        document.getElementById("area").innerHTML = "INVALID LENGTHS";
-        document.getElementById("angle-1").innerHTML = "N/A";
-        document.getElementById("angle-2").innerHTML = "N/A";
-        document.getElementById("angle-3").innerHTML = "N/A";
-        document.getElementById("triangle-type-2").innerHTML = "INVALID LENGTHS";
-
+        var length1 = Number(document.getElementById('length1').value);
+        var length2 = Number(document.getElementById('length2').value);
+        var length3 = Number(document.getElementById('length3').value);    
+        if (checkValid(length1, length2, length3) == 1) {
+            document.getElementById("triangle-type").innerHTML = triangleType(length1, length2, length3);
+            var perimeter_var = perimeter(length1, length2, length3);
+            document.getElementById("perimeter").innerHTML = perimeter_var;
+            document.getElementById("area").innerHTML = area(length1, length2, length3, perimeter_var);
+            var ang1 = angles(length1, length2, length3);
+            var ang2 = angles(length2, length3, length1);
+            var ang3 = angles(length3, length1, length2);
+            document.getElementById("angle-1").innerHTML = ang1;
+            document.getElementById("angle-2").innerHTML = ang2;
+            document.getElementById("angle-3").innerHTML = ang3;
+            document.getElementById("triangle-type-2").innerHTML = triangleTypeAng(ang1, ang2, ang3);
+        } else {
+            document.getElementById("triangle-type").innerHTML = "INVALID LENGTHS";
+            document.getElementById("perimeter").innerHTML = "INVALID LENGTHS";
+            document.getElementById("area").innerHTML = "INVALID LENGTHS";
+            document.getElementById("angle-2").innerHTML = "N/A";
+            document.getElementById("angle-3").innerHTML = "N/A";
+            document.getElementById("triangle-type-2").innerHTML = "INVALID LENGTHS";
+        }
     }
 }
 
 function checkValid(length1, length2, length3) {
     if ((length1+length2) >= length3 && (length2+length3) >= length1 && (length1+length3) >= length2){
-        return 1
+        return 1;
     } else {
-        return 0
+        return 0;
     }
 }
 
